c74993b54e3763209ddf860d495f2fd987b30b76
DmitriiSubpp <71903279+DmitriiSubpp@users.noreply.github.com>
2022-03-30 16:12:07 +0300

    Add files via upload

--- cshell.py
+++ cshell.py
@@ -1,35 +1,40 @@
 import subprocess
 import sys
+import os
 import socket
 import argparse
 import shlex
 
-def execute(cmd):
-    cmd = cmd.strip()
-    if not cmd:
-        return
-    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
-    return output.decode()
+def execute(cmd = ''):
+    try:
+        output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT, shell=True)
+        return output.decode()
+    except Exception as err:
+        print('Error in execute: ', err)
+        return str(err) + '\n'
 
 def start_process(args):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server = (args.target, args.port)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server = (args.target, args.port)
+        sock.bind(server)
+        sock.listen(1)
 
-    sock.bind(server)
-    sock.listen(1)
-
-    buf = b''
-
-    client, _ = sock.accept()
-
-    while True:
-        client.send(b'cmd:> ')
-        while '\n' not in buf.decode():
-            buf += client.recv(64)
-        output = execute(buf.decode())
-        client.send(output.encode())
         buf = b''
+
+        client, _ = sock.accept()
+
+        while True:
+            client.send(f'[{os.environ.get( "USERNAME" )}]-[{os.getcwd()}]$'.encode())
+            while '\n' not in buf.decode():
+                buf += client.recv(64)
+            output = execute(buf.decode())
+            client.send(output.encode())
+            buf = b''
+    except Exception as err:
+        print('Error in start_process: ', err)
+        sock.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='remote command shell')
@@ -37,7 +42,5 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', type=int, help='target port')
     args = parser.parse_args()
 
-    try:
-        start_process(args)
-    except Exception as err:
-        print('Error: ', err)
+    start_process(args)
+
