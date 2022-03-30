d4ce574021952cd2f56a2c63105617c7497b02e4
DmitriiSubpp <71903279+DmitriiSubpp@users.noreply.github.com>
2022-03-30 17:29:19 +0300

    Add files via upload

--- cshell.py
+++ cshell.py
@@ -5,15 +5,22 @@ import socket
 import argparse
 import shlex
 
-def execute(cmd = ''):
+def execute(cmd = '', work_dir = os.getcwd()):
     try:
-        output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT, shell=True)
-        return output.decode()
+        cmd = shlex.split(cmd)
+        if cmd[0] == 'cd':
+            work_dir = cmd[1]
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, cwd=work_dir)
+        return output.decode(), work_dir
     except Exception as err:
         print('Error in execute: ', err)
         return str(err) + '\n'
 
 def start_process(args):
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print(f'your ip: {ip}, hostname: {hostname}')
+    
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server = (args.target, args.port)
@@ -22,14 +29,14 @@ def start_process(args):
         sock.listen(1)
 
         buf = b''
-
+        work_dir = os.getcwd()
         client, _ = sock.accept()
 
         while True:
-            client.send(f'[{os.environ.get( "USERNAME" )}]-[{os.getcwd()}]$'.encode())
+            client.send(f'[{os.environ.get( "USERNAME" )}]-[{work_dir}]$'.encode())
             while '\n' not in buf.decode():
                 buf += client.recv(64)
-            output = execute(buf.decode())
+            output, work_dir = execute(buf.decode(), work_dir)
             client.send(output.encode())
             buf = b''
     except Exception as err:
